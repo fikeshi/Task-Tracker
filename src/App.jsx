cb145428c139deb26ab74ba025f6b4793d268992
kesh <efikunmi@yahoo.com>
2024-04-06 23:32:45 +0100

    made task dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from './components/AddTask'
@@ -9,25 +9,23 @@ import About from './components/About'
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: "true",
-    },
-    {
-      id: 2,
-      text: "Meeting at school",
-      day: "Feb 6th at 1:30pm",
-      reminder: "true",
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "Feb 5th at 2:30pm",
-      reminder: "false",
-    },
   ])
+
+  useEffect(()=>{
+     
+
+    fetchTasks()
+    
+  }, [])
+
+  // FetchTask
+  async function fetchTasks() {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+
+    return data
+  }
+
 
   // Add Task
   function addTask(task) {
